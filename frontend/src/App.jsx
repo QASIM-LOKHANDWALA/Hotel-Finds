9b13ad4145cd74f4ff4462d9877346680f8680c9
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-05-07 09:15:40 +0530

    Designed Hero Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { useLocation } from "react-router-dom";
+import { useLocation, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
     const isAdmin = useLocation().pathname.includes("admin");
 
-    return <div>{!isAdmin && <Navbar />}</div>;
+    return (
+        <div>
+            {!isAdmin && <Navbar />}
+            <div className="min-h-[70vh]">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </div>
+        </div>
+    );
 };
 
 export default App;
